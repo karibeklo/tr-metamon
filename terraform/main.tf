67f7ff95ea30501fa510df64409aa5d95b850eb6
karibeklo <karibe.k-lo@nhk.or.jp>
2025-07-29 18:11:33 +0900

    x-api-key も削除

--- terraform/main.tf
+++ terraform/main.tf
@@ -567,7 +567,7 @@ resource "aws_cloudfront_distribution" "metamon_distribution" {
 
     forwarded_values {
       query_string = true
-      headers      = ["Authorization", "X-API-Key"]
+      headers      = ["Authorization"]
 
       cookies {
         forward = "none"
